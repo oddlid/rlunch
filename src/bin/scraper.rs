c21a1a628e58632f5552fdd610196a5335aeaed0
Odd E. Ebbesen <oddebb@gmail.com>
2024-07-26 19:57:09 +0200

    Got fetching dishes from lindholmen.se half working

--- src/bin/scraper.rs
+++ src/bin/scraper.rs
@@ -1,3 +1,121 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{bail, Result};
+use lazy_static::lazy_static;
+use rlunch::data::*;
+use scraper::{selectable::Selectable, ElementRef, Html, Selector};
+
+// This is just a test bed for the first attempt at parsing lindholmen.se
+
+// Name your user agent after your app?
+static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+const UNDEF: &str = "UNDEFINED";
+
+fn sel(selector: &str) -> Selector {
+    Selector::parse(selector).unwrap()
+}
+
+lazy_static! {
+    static ref SEL_VIEW_CONTENT: Selector = sel("div.view-content");
+    static ref SEL_CONTENT: Selector = sel("div.content");
+    static ref SEL_TITLE: Selector = sel("h3.title");
+    static ref SEL_DISH_ROW: Selector = sel("div.table-list__row");
+    static ref SEL_DISH: Selector = sel("span.dish-name");
+    static ref SEL_DISH_NAME: Selector = sel("strong");
+    static ref SEL_DISH_TYPE: Selector = sel("div.icon-dish");
+    static ref SEL_DISH_PRICE: Selector = sel("div.table-list__column--price");
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = reqwest::Client::builder()
+        .user_agent(APP_USER_AGENT)
+        .build()?;
+    let res = client
+        .get("http://localhost:8080")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    let doc = Html::parse_document(&res);
+    // let vc = doc.select(&SEL_VIEW_CONTENT).next().unwrap();
+    let vc = match doc.select(&SEL_VIEW_CONTENT).next() {
+        Some(vc) => vc,
+        None => bail!("Invalid HTML"),
+    };
+    // for restaurant_block in vc.select(&SEL_TITLE) {
+    //     let name = restaurant_block
+    //         .text()
+    //         .map(|e| e.trim())
+    //         .collect::<Vec<_>>()
+    //         .join("");
+    //     println!("{}", name);
+    // }
+
+    // this works, for just getting the restaurant names
+    // let restaurants = vc
+    //     .select(&SEL_TITLE)
+    //     .map(|t| t.text().next().unwrap_or(UNDEF).trim())
+    //     .map(Restaurant::new)
+    //     .collect::<Vec<_>>();
+    // println!("{:#?}", restaurants);
+
+    // let mut site = Site::new("Lindholmen");
+    // This does absolutely not work!
+    // for titles in vc.select(&SEL_TITLE) {
+    //     let restaurant_name = titles.text().next().unwrap_or(UNDEF).trim();
+    //     println!("{restaurant_name}");
+    //     while let Some(s) = titles.next_sibling() {
+    //         println!("{:?}", s.value());
+    //     }
+    // }
+
+    for e in vc.child_elements() {
+        let dish_type = get_text(&e, &SEL_DISH_TYPE);
+        let (dish_name, dish_desc) = get_dish_name_and_desc(&e);
+        let dish_price = get_text(&e, &SEL_DISH_PRICE);
+        println!(
+            "=> Dish:\nType: {dish_type}\nName: {dish_name}\nDesc: {dish_desc}\nPrice: {dish_price}\n\n"
+        );
+    }
+
+    Ok(())
+}
+
+fn get_text(e: &ElementRef, sel: &Selector) -> String {
+    match e.select(sel).next() {
+        None => UNDEF.to_owned(),
+        Some(v) => match v.text().next() {
+            None => UNDEF.to_owned(),
+            Some(v) => v.trim().to_owned(),
+        },
+    }
+}
+
+fn reduce_whitespace(s: &str) -> String {
+    let mut result = String::with_capacity(s.len());
+    s.split_whitespace().for_each(|w| {
+        if !result.is_empty() {
+            result.push(' ');
+        }
+        result.push_str(w);
+    });
+    result
+}
+
+fn get_dish_name_and_desc(e: &ElementRef) -> (String, String) {
+    match e.select(&SEL_DISH).next() {
+        None => (UNDEF.to_owned(), UNDEF.to_owned()),
+        Some(v) => {
+            let mut t = v.text();
+            let name = match t.next() {
+                None => UNDEF.to_owned(),
+                Some(v) => v.trim().to_owned(),
+            };
+            let desc = match t.next() {
+                None => UNDEF.to_owned(),
+                Some(v) => reduce_whitespace(v),
+            };
+            (name, desc)
+        }
+    }
 }
--- src/data.rs
+++ src/data.rs
@@ -1,94 +1,234 @@
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
-use std::collections::{hash_map::HashMap, hash_set::HashSet};
+use std::{
+    collections::{hash_map::HashMap, hash_set::HashSet},
+    fmt::Display,
+};
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
 pub struct Dish {
     /// Name of the dish, e.g. "meatballs"
+    #[serde(default)]
     pub name: String,
     /// More details about the dish, e.g. "with spaghetti"
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
     // Extra info, e.g. "contains nuts"
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub comment: Option<String>,
     /// Optionals tags for filtering, e.g. "vego,gluten,lactose"
+    #[serde(default)]
     pub tags: HashSet<String>,
     /// Price, in whatever currency is in use
+    #[serde(default)]
     pub price: f32,
 }
 
-impl Default for Dish {
-    fn default() -> Self {
-        Self {
-            name: String::new(),
-            description: None,
-            comment: None,
-            tags: HashSet::new(),
-            price: 0.0,
+impl Display for Dish {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match &self.description {
+            Some(d) => write!(f, "{} {}", self.name, d),
+            None => write!(f, "{}", self.name),
         }
     }
 }
 
-#[allow(dead_code)]
 impl Dish {
-    fn new() -> Self {
-        Default::default()
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            ..Default::default()
+        }
     }
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
 pub struct Restaurant {
     /// Name of restaurant
+    #[serde(default)]
     pub name: String,
     /// Extra info
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub comment: Option<String>,
     /// Street address
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub address: Option<String>,
     /// Homepage
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub url: Option<String>,
     /// Google maps URL
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub map_url: Option<String>,
     /// When the scraping was last done
+    #[serde(default)]
     pub parsed_at: DateTime<Local>,
     /// List of current dishes
+    #[serde(default)]
     pub dishes: Vec<Dish>,
 }
 
-impl Default for Restaurant {
-    fn default() -> Self {
+impl Restaurant {
+    pub fn new(name: &str) -> Self {
         Self {
-            name: String::new(),
-            comment: None,
-            address: None,
-            url: None,
-            map_url: None,
+            name: name.to_owned(),
             parsed_at: Local::now(),
-            dishes: Vec::new(),
+            ..Default::default()
         }
     }
+
+    // unsure if I want to have methods like this
+    // pub fn add_dish(&mut self, dish: Dish) {
+    //     self.dishes.push(dish);
+    // }
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
 pub struct Site {
     /// Name of site/area
+    #[serde(default)]
     pub name: String,
     /// Extra info
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub comment: Option<String>,
-    /// Homepage
-    pub url: Option<String>,
     /// List of current restaurants
+    #[serde(default)]
     pub restaurants: HashMap<String, Restaurant>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+impl Site {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            ..Default::default()
+        }
+    }
+
+    // unsure if I want to have methods like this
+    // pub fn add_restaurant(&mut self, restaurant: Restaurant) {
+    //     self.restaurants.insert(restaurant.name.clone(), restaurant);
+    // }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
 pub struct City {
+    /// Name of city
+    #[serde(default)]
     pub name: String,
+    /// List of current sites
+    #[serde(default)]
+    pub sites: HashMap<String, Site>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+impl City {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            ..Default::default()
+        }
+    }
+
+    // unsure if I want to have methods like this
+    // pub fn add_site(&mut self, site: Site) {
+    //     self.sites.insert(site.name.clone(), site);
+    // }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
 pub struct Country {
+    /// Name of country
+    #[serde(default)]
     pub name: String,
-    pub currency: String,
+    /// Currency abbreviation to use as suffix for dish prices
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub currency_suffix: Option<String>,
+    /// List of current cities
+    #[serde(default)]
+    pub cities: HashMap<String, City>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
-pub struct LunchData {}
+impl Country {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            ..Default::default()
+        }
+    }
+
+    // unsure if I want to have methods like this
+    // pub fn add_city(&mut self, city: City) {
+    //     self.cities.insert(city.name.clone(), city);
+    // }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Default)]
+pub struct LunchData {
+    /// List of current countries
+    #[serde(default)]
+    pub countries: HashMap<String, Country>,
+}
+
+impl LunchData {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    // unsure if I want to have methods like this
+    // pub fn add_country(&mut self, country: Country) {
+    //     self.countries.insert(country.name.clone(), country);
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn dish_display() {
+        let d = Dish {
+            name: String::from("meat"),
+            description: Some(String::from("balls")),
+            ..Default::default()
+        };
+        assert_eq!("meat balls", format!("{d}"));
+    }
+
+    #[test]
+    fn dish_deserialize() {
+        let j = serde_json::json!({
+            "name": "Test",
+            "description": "with sauce",
+            "price": 32
+        });
+        let d: Dish = serde_json::from_value(j).unwrap();
+        assert_eq!(32.0, d.price);
+        println!("{d:#?}");
+    }
+
+    #[test]
+    #[ignore = "Visual inspection"]
+    fn show_structure() {
+        let mut d = Dish::new("meat");
+        d.description = Some(String::from("balls"));
+        d.price = 120.0;
+        d.tags.insert(String::from("carnivora"));
+        d.tags.insert(String::from("yummy"));
+
+        let mut r = Restaurant::new("Pasta House");
+        r.dishes.push(d);
+
+        let mut s = Site::new("SomeSite");
+        s.restaurants.insert(r.name.clone(), r);
+
+        let mut city = City::new("Göteborg");
+        city.sites.insert(s.name.clone(), s);
+
+        let mut country = Country::new("Sweden");
+        country.cities.insert(city.name.clone(), city);
+
+        let mut ld = LunchData::new();
+        ld.countries.insert(country.name.clone(), country);
+
+        println!("{}", serde_json::to_string_pretty(&ld).unwrap());
+    }
+}
