e49efb977a2e26af0ca09b265788004ba532373b
Odd E. Ebbesen <git@oddware.net>
2026-01-09 14:46:08 +0100

    Fixed readonly blunder in build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -3,8 +3,7 @@
 # This requires the following to have been run at some point before running this script:
 # docker buildx create --name builder --driver docker-container --use
 
-readonly _tag
-_tag=$(git describe --tags --abbrev=0 | sed 's/v//g')
+readonly _tag=$(git describe --tags --abbrev=0 | sed 's/v//g')
 docker buildx build \
   -t "oddlid/rlunch:$_tag" \
   --build-arg VERSION="$_tag" \
